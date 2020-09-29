f74308868bee45a4365acaf594cac07469b65dc5
user <user@EF04>
2020-09-29 09:42:06 +0800

    lab1

---
+++ src/lab3/Main.java
@@ -0,0 +1,10 @@
+package lab3;
+
+public class Main {
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("I'm Luo Yuxiang");
+    System.out.println("My nid is D0649914");
+    System.out.println("This is lab 1 hw");
+  }
+}
